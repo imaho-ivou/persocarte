af9e6030b05c38a131e57c14460f31f272558e36
kevin imaho <kevin.imaho@gmail.com>
2022-01-06 12:54:03 +0400

    section texte 1 +section texte 2 +section texte 3

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,119 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(),
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/face15.jpg'),
+                  backgroundColor: Colors.black,
+                  radius: 100,
+                ),
                 Container(
-                  child: Column(
-                    children: [
-                      Text('text'),
-                      SizedBox(),
-                    ],
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    'Kévin Imaho',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontFamily: 'Pacifico-Regular'),
                   ),
-                )
+                ),
+                const Divider(
+                  height: 20,
+                  thickness: 2,
+                  indent: 250,
+                  endIndent: 250,
+                  color: Colors.white,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30.0),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Card(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 50),
+                                  child: Icon(
+                                    Icons.phone,
+                                    size: 35.0,
+                                    color: Colors.teal,
+                                  ),
+                                ),
+                                Text(
+                                  '0692 00 00 00',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 25,
+                                      fontFamily: 'SourceSansPro-Regular'),
+                                ),
+                              ],
+                            ),
+                          ),
+                          height: 90.0,
+                          width: 750.0,
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 30),
+                          child: Card(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 50),
+                                  child: Icon(
+                                    Icons.email,
+                                    size: 35.0,
+                                    color: Colors.teal,
+                                  ),
+                                ),
+                                Text(
+                                  'kevin.imaho@gmail.com',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 25,
+                                      fontFamily: 'SourceSansPro-Regular'),
+                                ),
+                              ],
+                            ),
+                          ),
+                          height: 90.0,
+                          width: 750.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Divider(
+                  height: 20,
+                  thickness: 2,
+                  indent: 250,
+                  endIndent: 250,
+                  color: Colors.white,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      child: Text(
+                        'Formation 2021-2022',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontFamily: 'Pacifico-Regular'),
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        'Web developer',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontFamily: 'Pacifico-Regular'),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
